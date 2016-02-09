6a3ed86a4a5be09759a85cf9a105151090b3d308
brendon-boldt <brendon_boldt@yahoo.com>
2016-02-08 23:12:43 -0500

    Further experiements with different filter numbers

--- mnist_image.py
+++ mnist_image.py
@@ -36,42 +36,38 @@ def generate_image(num):
 # For now, I think the batch size should equal the class size
 #batch_size = 1
 class_size = 10
-fc_size = 1024
-conv1_size = 32
-conv2_size = 64
+fc_size = 64
+conv1_size = 16 
+# Figure out 
+conv2_size = 2
+iterations = 1000
 
-
-# Figure where I should use batch_size and where I should use class_size
 x  = tf.placeholder("float", shape=[class_size, 784])
-'''
-  y_ = tf.placeholder("float", shape=[None, 10 ])
-  W_fc2 = weight_variable([10, 1024])
-  b_fc2 = bias_variable([10]) # I might want to uses biases differently
-'''
 y_ = tf.placeholder("float", shape=[class_size, class_size])
 W_fc2 = weight_variable([class_size, fc_size])
 b_fc2 = bias_variable([class_size])
-h_fc1 = tf.nn.relu(tf.matmul(y_ + b_fc2, W_fc2))
 W_fc1 = weight_variable([fc_size, 7 * 7 * conv2_size])
 b_fc1 = bias_variable([fc_size])
-h_pool2_flat = tf.matmul(h_fc1 + b_fc1, W_fc1)
-h_pool2 = tf.reshape(h_pool2_flat, [-1, 7, 7, conv2_size]) # I think that's right...
-h_conv2 = tf.image.resize_images(h_pool2,14,14, method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
 W_conv2 = weight_variable([5,5,conv1_size,conv2_size])
 b_conv2 = bias_variable([conv2_size])
-'''
-  print(h_conv2.get_shape())
-  print((h_conv2 + b_conv2).get_shape())
-  print([class_size,14,14,conv1_size])
-  exit()
-'''
-h_pool1 = nn_ops.conv2d_transpose(h_conv2 + b_conv2,W_conv2,
-    [class_size,14,14,conv1_size],[1,1,1,1])
-h_conv1 = tf.image.resize_images(h_pool1 ,28,28,method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
 W_conv1 = weight_variable([5,5,1,conv1_size])
 b_conv1 = bias_variable([conv1_size])
-x_image = nn_ops.conv2d_transpose(h_conv1 + b_conv1, W_conv1, [class_size,28,28,1], [1,1,1,1])
-x_image = tf.nn.relu(x_image)
+
+# I need to figure out where relu ought to be used
+# I might find it helpful to use tanh to make the filters more black and white
+def rebuild_image():
+  h_fc1 = tf.nn.relu(tf.matmul(y_ + b_fc2, W_fc2))
+  h_pool2_flat = tf.matmul(h_fc1 + b_fc1, W_fc1)
+  h_pool2 = tf.reshape(h_pool2_flat, [-1, 7, 7, conv2_size]) # I think that's right...
+  h_conv2 = tf.image.resize_images(h_pool2,14,14, method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+  h_pool1 = nn_ops.conv2d_transpose(h_conv2 + b_conv2,W_conv2,
+      [class_size,14,14,conv1_size],[1,1,1,1])
+  h_conv1 = tf.image.resize_images(h_pool1 ,28,28,method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+  x_image = nn_ops.conv2d_transpose(h_conv1 + b_conv1, W_conv1, [class_size,28,28,1], [1,1,1,1])
+  x_image = tf.nn.relu(x_image)
+  return x_image
+
+x_image = rebuild_image()
 
 ########
 
@@ -81,7 +77,7 @@ train_step = tf.train.AdamOptimizer(1e-4).minimize(error)
 sess.run(tf.initialize_all_variables())
 
 # Put the file names of the images in order
-image_names = ['three.gz'] * class_size
+image_names = list(map(lambda x: "train_images/" + str(x) + ".gz", range(class_size)))
 images = np.array(list(map(lambda x: mo.idx_to_array(x), image_names)))
 images = np.reshape(images, [class_size, 28*28])
 #images = np.reshape(np.array([mo.idx_to_array('three.gz')]), [1,-1])/255.0
@@ -89,13 +85,24 @@ images = np.reshape(images, [class_size, 28*28])
 oh_encodings = np.identity(class_size)
 #fd = {x:three_im}
 
-for i in range(30):
+for i in range(iterations):
   if i % 20 == 0:
     print("step %d"%(i,))
     #print(tf.reduce_max(x_image).eval(feed_dict={y_:[oh_encodings[0]]}))
     train_accuracy = error.eval(feed_dict={x:images , y_:oh_encodings})
     print("CE: %s"%(train_accuracy,))
   train_step.run(feed_dict={x:images , y_:oh_encodings})
+
+'''
+  W_fc2 = weight_variable([class_size, fc_size])
+  b_fc2 = bias_variable([class_size])
+  h_fc1 = tf.nn.relu(tf.matmul(y_ + b_fc2, W_fc2))
+  W_fc1 = weight_variable([fc_size, 7 * 7 * conv2_size])
+'''
+
+#W_fc1 = tf.random_shuffle(W_fc1)
+#x_image = rebuild_image()
+
 
 dir = "result_images/"
 x_arrays = (255.0*np.clip(x_image.eval(session=sess, feed_dict={y_:oh_encodings}), 0.0, 1.0)).astype(np.uint8)
@@ -107,19 +114,34 @@ for x_array in x_arrays:
     file.write(tf.image.encode_png(x_image).eval(session=sess))
     class_index += 1
 
+W_conv1_t = tf.transpose(W_conv1, perm=[3,0,1,2])
 
-exit()
-#W_conv1_t = tf.transpose(W_conv1, perm=[3,0,1,2])
-#W_conv2_t = tf.reduce_sum(tf.transpose(W_conv2, perm=[3,0,1,2]), [3], keep_dims=True)
+'''
+  h_pool1 = nn_ops.conv2d_transpose(h_conv2 + b_conv2,W_conv2,
+      [class_size,14,14,conv1_size],[1,1,1,1])
+'''
+index = 0
+dir = "l1f/"
+for t in W_conv1_t.eval():
+  with open(dir+'filter'+str(index)+'.png', "wb") as file:
+    t = tf.constant(t)
+    #t = tf.expand_dims(tf.constant(t), 0)
+    #t_n = tf.squeeze(nn_ops.conv2d_transpose(t, W_conv1, [1,5,5,1],[1,1,1,1]), [0])
+    t = tf.image.resize_images(t,50,50, method=tf.image.ResizeMethod.BICUBIC)
+    t = tf.constant(ops.normalize(t.eval(), 0, 255))
+    file.write(tf.image.encode_png(t).eval())
+  index += 1
+
+W_conv2_t = tf.transpose(W_conv2, perm=[3,0,1,2])
 
 index = 0
-dir = "filters/"
+dir = "l2f/"
 #for t in W_conv1_t.eval():
 for t in W_conv2_t.eval():
   with open(dir+'filter'+str(index)+'.png', "wb") as file:
-    t = tf.constant(t)
-    t_n = tf.image.resize_images(t,50,50, method=tf.image.ResizeMethod.BICUBIC)
+    t = tf.expand_dims(tf.constant(t), 0)
+    t_n = tf.squeeze(nn_ops.conv2d_transpose(t, W_conv1, [1,5,5,1],[1,1,1,1]), [0])
+    t_n = tf.image.resize_images(t_n,50,50, method=tf.image.ResizeMethod.BICUBIC)
     t_n = tf.constant(ops.normalize(t_n.eval(), 0, 255))
-    #t_n = t_map(lambda x: numpy.uint8(x), t_n)
     file.write(tf.image.encode_png(t_n).eval())
   index += 1
